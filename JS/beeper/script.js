881c4d29d37e563104981f228c490f1b0da91912
ThiagoAC1 <163343251+ThiagoAC1@users.noreply.github.com>
2024-11-02 13:24:11 -0300

    Add files via upload

---
+++ JS/beeper/script.js
@@ -0,0 +1,7 @@
+function seEjecutaEnEvento() {
+    document.body.insertAdjacentHTML('beforeend', '<p>BEEP</p>');
+    
+    document.body.classList.toggle('color');
+}
+
+document.getElementById('beepButton').addEventListener('click', seEjecutaEnEvento);
